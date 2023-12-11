7d6a5421961fa7cca77f97d4fe1de80aa9d2456f
Sanatan Kafle <kaflesanatan100@gmail.com>
2023-12-11 20:37:03 +0545

    Added files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".writing-silde-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
