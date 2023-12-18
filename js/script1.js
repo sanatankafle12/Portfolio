24463d75c29890ce5f9f62ee7f9b2b4583efcd97
Sanatan Kafle <kaflesanatan100@gmail.com>
2023-12-18 09:48:11 +0545

    Replaced files

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('next-1').onclick = function(){
-    let lists = document.querySelectorAll('.item-1');
-    document.getElementById('slide-1').appendChild(lists[0]);
-}
-document.getElementById('prev-1').onclick = function(){
-    let lists = document.querySelectorAll('.item-1');
-    document.getElementById('slide-1').prepend(lists[lists.length - 1]);
-}
-  
